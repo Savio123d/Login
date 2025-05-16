fd7d879bc8c22961f133410f4a7fa31b4d542f3c
Savio123d <savio13foxy@gmail.com>
2025-05-15 21:31:18 -0300

    Update cadastro.js

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,14 +1,21 @@
 const botao = document.getElementById('btnCadastrar');
+
 const listaUsuario = [];
-botao.addEventListener('click',function(){
 
-const usuario = {
-    usuario: document.getElementById('login').value,
-    senha : document.getElementById('senha').value
+botao.addEventListener('click', function () {
 
-};
-console.log(usuario)
-listaUsuario.push(usuario);
-listaJson = JSON.stringify(listaUsuario);
-localStorage.setItem("usuarios",listaJson);
+    const usuario = {
+        usuario: document.getElementById('login').value,
+        senha: document.getElementById('senha').value
+
+    };
+    console.log(usuario)
+    listaUsuario.push(usuario);
+    listaJson = JSON.stringify(listaUsuario);
+    localStorage.setItem("usuarios", listaJson);
 });
+
+function listar(){
+    const usuarioCadstrados = JSON.parse(localStorage.getItem("usuarios") || []);
+    
+}

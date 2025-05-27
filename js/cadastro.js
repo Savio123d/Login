0b0856e4891eda59f271acb22c6d85f203c57d68
Savio123d <savio13foxy@gmail.com>
2025-05-26 21:10:13 -0300

    Update cadastro.js

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,55 +1,79 @@
-// cadastro com crud
+//cadastro com crud
 
+//criação de registro
 const botao = document.getElementById('btnCadastrar');
-const listaUsuario = [];
+// const listaUsuario = [];
 
-// criacao registro
+//criação registro
 botao.addEventListener('click', function () {
+    listaUsuario = JSON.parse(localStorage.getItem("usuarios")) || [];
     const usuario = {
         usuario: document.getElementById('login').value,
         senha: document.getElementById('senha').value
     };
+
+    //criar lista de usuarios
     //console.log(usuario);
     listaUsuario.push(usuario);
     listaJson = JSON.stringify(listaUsuario);
     localStorage.setItem("usuarios", listaJson);
+    document.getElementById('login').value = '';
+    document.getElementById('senha').value = '';
     listar();
 });
 
 function listar() {
-    const usuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
-    let tabela = document.getElementById('lista_usuarios');
+    const usuarioCadastrados = JSON.parse(localStorage.getItem("usuarios")) || []; //  || SE é a minha memoria estiver vazia, eu não precisado converter em JSON);
+
+    let tabela = document.getElementById('lista_usuario');
     tabela.innerHTML = '';
-    usuariosCadastrados.forEach((usuario, index) => {
+    usuarioCadastrados.forEach((usuario, index) => { //forEach é um for que passa por parametros Usuario e o index;
         let linha = document.createElement('tr');
 
+        // imprimir conteudo cadastrado
         linha.innerHTML = `
-            <td>${usuario.usuario}</td>
-            <td>${usuario.senha}</td>
-            <td>
-                <button onclick="editarUsuario(${index})">Editar</button>
-                <button onclick="excluirUsuario(${index})">Excluir</button>
-            </td>
+        <td>${usuario.usuario}</td>
+        <td>${usuario.senha}</td>
+        
+        <td>
+        <button onclick = "editarUsuario(${index})"> Editar  
+        <button onclick = "excluirUsuario(${index})"> Excluir </button>
+        
+        </button></td>
         `;
+
         tabela.appendChild(linha);
+
     });
 }
 
-function editarUsuario(index){
+function editarUsuario() {
+    const usuarioCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-    const usuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
-    usuariosCadastrados.splice(index,1);
+    usuarioCadastrados[index];
+    const objUsuario = usuarioCadastrados[index];
+    document.getElementById('login').value = objUsuario.usuario;
+    document.getElementById('senha').value = objUsuario.senha;
     
+
 }
 
 function excluirUsuario(index) {
-    const usuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
-       
-    if (confirm("Voce realemnte quer excluir? ")) {
-        usuariosCadastrados.splice(index,1);
-        listaJson = JSON.stringify(usuariosCadastrados);
-        localStorage.setItem("usuarios",listaJson);
+    //vou excluir diretamente do index, por contem o conteudo que o usuario digitou
+    //pegamos ele da linha 37, onde preenchemos ele com um dados
+
+    const usuarioCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    // 1 - excluir usuario 
+    // 2 - converter para JSON
+    // 3 - sobrescrever o que estava no 1.
+
+    if (confirm("Você realmente quer excluir?")) {
+    /* 1 */ usuarioCadastrados.splice(index, 1);  // splice procura um indice do meu vetor, e extrai desse indece a posição que eu achei. 1 item da lista.
+    /* 2 */ listaJson = JSON.stringify(usuarioCadastrados); // aqui vamos atualizar a minha memoria, depois de excluir um usuario.
+    /* 3 */ localStorage.setItem("usuarios", listaJson); // sobrescreve oque estava antes.
     }
+
     listar();
 }
 
